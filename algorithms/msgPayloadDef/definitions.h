68dd9985c962244d7c5e7d7807b8c8091c3d71ef
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-08-20 13:10:27 -0600

    Type the last of the message payload constants

--- algorithms/msgPayloadDef/definitions.h
+++ algorithms/msgPayloadDef/definitions.h
@@ -10,10 +10,6 @@
 #error "mission/parameters.h must define MAX_NUM_CSS_SENSORS, MAX_EFF_CNT and RW_EFF_CNT."
 #endif
 
-// FP32 algorithm specific constants
-#define MAX_MEASUREMENT_NUMBER 5
-#define MAX_MEASUREMENT_VECTOR 5
-
 // The plain C boundary types size their arrays from the mission/parameters.h macros, because a C
 // array bound has to be an integer constant expression and a const variable is not one in C. Every
 // C++ translation unit should reach for the typed constants below instead.
@@ -25,6 +21,10 @@ inline constexpr std::uint32_t kMaxNumCssSensors = MAX_NUM_CSS_SENSORS;
 inline constexpr std::uint32_t kMaxThrusterCount = MAX_EFF_CNT;
 inline constexpr std::uint32_t kMaxNumRw = RW_EFF_CNT;  //!< [-] maximum number of reaction wheels
 inline constexpr std::uint32_t kMimuCount = 3U;
+
+// FP32 algorithm specific constants
+inline constexpr std::uint32_t kMaxMeasurementNumber = 5U;
+inline constexpr std::uint32_t kMaxMeasurementVector = 5U;
 
 #endif  // __cplusplus
 
