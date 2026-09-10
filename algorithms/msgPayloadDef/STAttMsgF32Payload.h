3c6bfb8c0f165fb844e06be7ae804d1297b2db99
tteil <thibaud.teil@lasp.colorado.edu>
2026-09-10 14:38:58 -0600

    Change timetag to double in filtermsgs

--- algorithms/msgPayloadDef/STAttMsgF32Payload.h
+++ algorithms/msgPayloadDef/STAttMsgF32Payload.h
@@ -3,7 +3,7 @@
 
 /*! @brief Output structure for ST attitude measurement in vehicle body frame*/
 typedef struct {
-    float timeTag;          //!< [s] Vehicle time code associated with measurement
+    double timeTag;         //!< [s] Vehicle time code associated with measurement
     float MRP_BdyInrtl[3];  //!< [-] MRP estimate of inertial to body transformation
     float omega_BN_B[3];    //!< [rad/s] Platform inertial angular velocity
     float dcm_CB[9];        //!< Star Tracker mount frame in the body frame
--- algorithms/msgPayloadDef/STSensorMsgF32Payload.h
+++ algorithms/msgPayloadDef/STSensorMsgF32Payload.h
@@ -3,7 +3,7 @@
 
 /*! @brief Star tracker sensor message (F32) */
 typedef struct {
-    float timeTag;         //!< [s] Time tag placed on the output state
+    double timeTag;        //!< [s] Time tag placed on the output state
     float qInrtl2Case[4];  //!< [-] Quaternion to go from the inertial to case
     float omega_CN_C[3];   //!< [rad/s] Inertial angular velocity in case frame
 } STSensorMsgF32Payload;
