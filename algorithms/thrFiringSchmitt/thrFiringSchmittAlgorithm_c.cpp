lasp/fp32-fsw-xmera

Add a validateConfig function to the thrFiringSchmitt C shim

algorithms/thrFiringSchmitt/thrFiringSchmittAlgorithm_c.cpp
@@ -1,5 +1,6 @@
 #include "thrFiringSchmittAlgorithm_c.h"
 #include "thrFiringSchmittAlgorithm.h"
+#include "utilities/fsw/freestandingInvalidArgument.h"
 #include "utilities/fsw/opaqueHandle.h"
 
 #include <algorithm>
@@ -38,6 +39,32 @@ ThrFiringSchmittConfig configFromC(const uint32_t numThrusters,
 }  // namespace
 
 uint32_t ThrFiringSchmittAlgorithm_getMaxThrusterCount(void) { return THR_FIRING_SCHMITT_MAX_THRUSTER_COUNT; }
+
+bool ThrFiringSchmittAlgorithm_validateConfig(const uint32_t numThrusters,
+                                              float maxThrust[THR_FIRING_SCHMITT_MAX_THRUSTER_COUNT],
+                                              const float levelOn,
+                                              const float levelOff,
+                                              const float thrMinFireTime,
+                                              const float controlPeriod,
+                                              const float onTimeSaturationFactor,
+                                              const ThrFiringSchmittPulsingRegime pulsingRegime) {
+    // Attempt to build the config through the real create path (configFromC ->
+    // ThrFiringSchmittConfig::create): success means valid, a throw means invalid.
+    // Reusing create means this validation can never drift from the rules it enforces.
+    try {
+        (void)configFromC(numThrusters,
+                          maxThrust,
+                          levelOn,
+                          levelOff,
+                          thrMinFireTime,
+                          controlPeriod,
+                          onTimeSaturationFactor,
+                          pulsingRegime);
+        return true;
+    } catch (const fsw::invalid_argument&) {
+        return false;
+    }
+}
 
 ThrFiringSchmittAlgorithmHandle* ThrFiringSchmittAlgorithm_create(
     const uint32_t numThrusters,

algorithms/thrFiringSchmitt/thrFiringSchmittAlgorithm_c.h
@@ -3,6 +3,7 @@
 
 #include "thrFiringSchmittTypes.h"
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -17,6 +18,28 @@ typedef struct ThrFiringSchmittAlgorithmHandle ThrFiringSchmittAlgorithmHandle;
  * @return The maximum thruster count (THR_FIRING_SCHMITT_MAX_THRUSTER_COUNT).
  */
 uint32_t ThrFiringSchmittAlgorithm_getMaxThrusterCount(void);
+
+/**
+ * @brief Report whether a configuration would be accepted by create/setConfig.
+ * @param numThrusters           [-] Number of thrusters on the vehicle.
+ * @param maxThrust              [N] Per-thruster maximum thrust; the first numThrusters entries are used.
+ * @param levelOn                [-] ON duty cycle fraction threshold, in (0, 1].
+ * @param levelOff               [-] OFF duty cycle fraction threshold, in [0, 1).
+ * @param thrMinFireTime         [s] Minimum commandable thruster fire time.
+ * @param controlPeriod          [s] Control period over which the force command applies.
+ * @param onTimeSaturationFactor [-] Control-period multiplier applied when on-time saturates.
+ * @param pulsingRegime          [-] On-pulsing or off-pulsing.
+ * @return true if the configuration is valid. Never throws, so it can guard the
+ *         throwing create/setConfig from an invalid configuration.
+ */
+bool ThrFiringSchmittAlgorithm_validateConfig(uint32_t numThrusters,
+                                              float maxThrust[THR_FIRING_SCHMITT_MAX_THRUSTER_COUNT],
+                                              float levelOn,
+                                              float levelOff,
+                                              float thrMinFireTime,
+                                              float controlPeriod,
+                                              float onTimeSaturationFactor,
+                                              ThrFiringSchmittPulsingRegime pulsingRegime);
 
 /**
  * @brief Construct a new ThrFiringSchmittAlgorithm instance from the supplied configuration.

lasp/fp32-fsw-ximera

algorithms/thrFiringSchmitt/thrFiringSchmittAlgorithm_c.cpp
@@ -1,6 +1,7 @@
 #include "thrFiringSchmittAlgorithm_c.h"
 #include "msgPayloadDef/definitions.h"
 #include "thrFiringSchmittAlgorithm.h"
+#include "utilities/fsw/freestandingInvalidArgument.h"
 #include "utilities/fsw/opaqueHandle.h"
 
 #include <algorithm>
@@ -36,6 +37,32 @@ ThrFiringSchmittConfig configFromC(const uint32_t numThrusters,
 }  // namespace
 
 uint32_t ThrFiringSchmittAlgorithm_getMaxThrusterCount(void) { return kMaxThrusterCount; }
+
+bool ThrFiringSchmittAlgorithm_validateConfig(const uint32_t numThrusters,
+                                              float maxThrust[MAX_EFF_CNT],
+                                              const float levelOn,
+                                              const float levelOff,
+                                              const float thrMinFireTime,
+                                              const float controlPeriod,
+                                              const float onTimeSaturationFactor,
+                                              const ThrFiringSchmittPulsingRegime pulsingRegime) {
+    // Attempt to build the config through the real create path (configFromC ->
+    // ThrFiringSchmittConfig::create): success means valid, a throw means invalid.
+    // Reusing create means this validation can never drift from the rules it enforces.
+    try {
+        (void)configFromC(numThrusters,
+                          maxThrust,
+                          levelOn,
+                          levelOff,
+                          thrMinFireTime,
+                          controlPeriod,
+                          onTimeSaturationFactor,
+                          pulsingRegime);
+        return true;
+    } catch (const fsw::invalid_argument&) {
+        return false;
+    }
+}
 
 ThrFiringSchmittAlgorithmHandle* ThrFiringSchmittAlgorithm_create(
     const uint32_t numThrusters,

algorithms/thrFiringSchmitt/thrFiringSchmittAlgorithm_c.h
@@ -3,6 +3,7 @@
 
 #include "thrFiringSchmittTypes.h"
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -17,6 +18,28 @@ typedef struct ThrFiringSchmittAlgorithmHandle ThrFiringSchmittAlgorithmHandle;
  * @return The maximum thruster count (kMaxThrusterCount).
  */
 uint32_t ThrFiringSchmittAlgorithm_getMaxThrusterCount(void);
+
+/**
+ * @brief Report whether a configuration would be accepted by create/setConfig.
+ * @param numThrusters           [-] Number of thrusters on the vehicle.
+ * @param maxThrust              [N] Per-thruster maximum thrust; the first numThrusters entries are used.
+ * @param levelOn                [-] ON duty cycle fraction threshold, in (0, 1].
+ * @param levelOff               [-] OFF duty cycle fraction threshold, in [0, 1).
+ * @param thrMinFireTime         [s] Minimum commandable thruster fire time.
+ * @param controlPeriod          [s] Control period over which the force command applies.
+ * @param onTimeSaturationFactor [-] Control-period multiplier applied when on-time saturates.
+ * @param pulsingRegime          [-] On-pulsing or off-pulsing.
+ * @return true if the configuration is valid. Never throws, so it can guard the
+ *         throwing create/setConfig from an invalid configuration.
+ */
+bool ThrFiringSchmittAlgorithm_validateConfig(uint32_t numThrusters,
+                                              float maxThrust[MAX_EFF_CNT],
+                                              float levelOn,
+                                              float levelOff,
+                                              float thrMinFireTime,
+                                              float controlPeriod,
+                                              float onTimeSaturationFactor,
+                                              ThrFiringSchmittPulsingRegime pulsingRegime);
 
 /**
  * @brief Construct a new ThrFiringSchmittAlgorithm instance from the supplied configuration.